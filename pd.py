0b33ba7f5c87fc0791488ada219cb0094e0474fa
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-12-01 18:02:32 +0100

    indentare su vulcan e' un cancro

--- pd.py
+++ pd.py
@@ -1,11 +1,15 @@
+
 import subprocess
 import csv
 import time
 from collections import Counter
 import json
+from tqdm import tqdm
+
 authors_res = {}
+
 with open("authors.txt", "r") as input_file:
-    for query_term in input_file:
+    for query_term in tqdm(input_file, desc="Processing query"):
         time.sleep(1)
         query = f'{query_term.strip()} [AUTH]'
         command = f'esearch -db pubmed -query "{query}" | efetch -format docsum | xtract -pattern DocumentSummary -element FullJournalName'
@@ -15,6 +19,10 @@ with open("authors.txt", "r") as input_file:
             result = result.strip().split('\n')
             counter = Counter(result)
             authors_res[query_term.strip()] = counter.most_common(3)
+
+            with open("Risultati.csv", "a") as ris:
+                ris.write(query_term.strip() + ',' + ','.join(str(item) for item in authors_res[query_term.strip()]) + '\n')
+
             if not result:
                 print(f"No result for: {query_term.strip()}")
 
@@ -25,6 +33,4 @@ with open("authors.txt", "r") as input_file:
 with open("authors_results.json", "w") as json_file:
     json.dump(authors_res, json_file, indent=2)
 
-print("Results saved to authors_results.json")
-
-
+print("Results saved to authors_results.json and Risultati.csv")
