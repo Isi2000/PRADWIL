d4177e0d417299722bbcdf49266cff85aa4d2c3b
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-12-13 16:37:08 +0100

    Scatter plots

--- scatter_remondini.py
+++ scatter_remondini.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Assuming data is loaded into NumPy arrays
-eig = np.load("./results/eigenvector_centrality.npy")
-deg = np.load("./results/degree_centrality.npy")
-bet = np.load("./results/betweeness_centrality.npy")
-clo = np.load("./results/closeness_centrality.npy")
+eig = np.load("./results/eigenvector_centrality_cleaned.npy")
+deg = np.load("./results/degree_centrality_cleaned.npy")
+bet = np.load("./results/betweenness_centrality_cleaned.npy")
+clo = np.load("./results/closeness_centrality_cleaned.npy")
 
 # Mapping colors to centrality measures
 centrality_colors = {
@@ -39,4 +39,4 @@ plot_scatter(axs[1, 0], bet, 'Betweenness Centrality', centrality_colors['bet'])
 plot_scatter(axs[1, 1], clo, 'Closeness Centrality', centrality_colors['clo'])
 
 plt.tight_layout()
-plt.savefig('./images/scatter_remondini.png')
+plt.savefig('./images/scatter_remondini_cleaned.png')
